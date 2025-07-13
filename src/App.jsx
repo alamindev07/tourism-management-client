172cb967c423a469da91bb0678cef4c5e74917f9
alamindev07 <alamin.dev07@gmail.com>
2025-07-13 19:25:22 +0600

    Basic Folder structure done

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      
+      <h1 className='text-4xl'>Tourism Management Project Folder Structure setup done</h1>
+      
+    </>
+  )
+}
+
+export default App
